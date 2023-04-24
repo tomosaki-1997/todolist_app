adc4f8db3e1b09a778ea860d465c3f302cc8aacd
yuichirotanaka <tanaka123305@icloud.com>
2023-04-24 13:04:40 +0900

    マイグレーション修正

--- database/migrations/2023_04_09_131531_add_user_id_to_folders.php
+++ database/migrations/2023_04_09_131531_add_user_id_to_folders.php
@@ -14,10 +14,10 @@ class AddUserIdToFolders extends Migration
     public function up()
     {
         Schema::table('folders', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned();
+            //$table->integer('user_id')->unsigned();
 
             // 外部キーを設定する
-            $table->foreign('user_id')->references('id')->on('users');
+            //$table->foreign('user_id')->references('id')->on('users');
         });
     }
 
